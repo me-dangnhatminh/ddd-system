23f54807886108414291e98c702a5bb266762dcd
me-dangnhatminh <dangnhatminh09032002@gmail.com>
2024-02-01 18:25:48 +0700

    Add ErrorType constants

---
+++ src/common/constants/error-type.ts
@@ -0,0 +1,11 @@
+export const ErrorType = {
+  InternalServerError: 'InternalServerError',
+  InvalidOperation: 'InvalidOperation',
+  InvalidParameter: 'InvalidParameter',
+  Unauthorized: 'Unauthorized',
+  Forbidden: 'Forbidden',
+  Conflict: 'Conflict',
+  BadRequest: 'BadRequest',
+  NotFound: 'NotFound',
+  UnprocessableEntity: 'UnprocessableEntity',
+};
