220f59aefecf55139cb2bd88502778adb842090e
me-dangnhatminh <dangnhatminh09032002@gmail.com>
2024-03-25 12:32:31 +0700

    Refactor room.interface.ts to import IParticipantOfRoom instead of IAnonymousOfRoom and IGuestOfRoom

--- src/modules/meeting/domain/interfaces/room.interface.ts
+++ src/modules/meeting/domain/interfaces/room.interface.ts
@@ -1,7 +1,4 @@
-import {
-  IAnonymousOfRoom,
-  IGuestOfRoom,
-} from './participant-of-room.interface';
+import { IParticipantOfRoom } from './participant-of-room.interface';
 import { IParticipant } from './participant.interface';
 
 export enum RoomStates {
@@ -29,28 +26,4 @@ export abstract class IRoom {
   protected participants: Map<IParticipant['id'], IParticipant>;
 
   abstract getParticipant(partId: string): IParticipantOfRoom;
-}
-
-export abstract class IParticipantOfRoom extends IRoom {
-  protected info: IParticipant;
-}
-
-export abstract class IPublicRoom extends IRoom {
-  abstract registerPart(part: IParticipant): IParticipantOfRoom;
-}
-
-export abstract class IOwerOfRoom extends IRoom {
-  abstract kickPart(part: IGuestOfRoom | IAnonymousOfRoom): void;
-  abstract banPart(part: IGuestOfRoom | IAnonymousOfRoom): void;
-  abstract unbanPart(part: IAnonymousOfRoom): void;
-
-  abstract closeRoom(): void;
-  abstract lockRoom(): void;
-  abstract unlockRoom(): void;
-}
-
-export class Room {
-  static registerPart(par: IParticipant): IParticipantOfRoom {
-    throw new Error('Method not implemented.');
-  }
 }
